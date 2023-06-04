46e560caaef818f22c7f0bdd49a1a30d6d2d4ea5
Joao Franca <jvictorfranca@yahoo.com.br>
2023-06-04 11:47:54 -0300

    adds plotly

--- Plots/Scatter/scatter_matrix_px.py
+++ Plots/Scatter/scatter_matrix_px.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
 credit_data = pd.read_csv('Datasets\credit_data.csv')
 
 print(credit_data.columns)
 
-scatter_matrix_plot = px.scatter_matrix(credit_data, dimensions=['age', 'income', 'loan'], color='default')
-scatter_matrix_plot.write_html('./scatter_matrix_px.html')
+credit_data['color_labels'] = np.where(credit_data['default'] == 1, "Defaulted", "Not_Defaulted" )
+
+scatter_matrix_plot = px.scatter_matrix(credit_data, dimensions=['age', 'income', 'loan'], color='color_labels', color_continuous_scale=False)
+scatter_matrix_plot.write_html('./Plots/Scatter/scatter_matrix_px.html')
